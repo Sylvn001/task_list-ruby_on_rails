3b98d384ccf68145fd0f604b7fc63cf9e52d9e8b
Sylvn001 <juniorbaarros@gmail.com>
2021-10-20 12:35:35 -0300

    Created users_backoffice and auth protection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
-class Profile < ApplicationRecord
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 end
